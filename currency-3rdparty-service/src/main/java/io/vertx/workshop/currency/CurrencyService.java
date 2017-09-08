e9fbbb53ff048543463c3ff1d7dac767e108e2a1
Clement Escoffier <clement.escoffier@gmail.com>
2017-09-08 10:52:16 +0200

    Implement the voluntary broken currency service

---
+++ currency-3rdparty-service/src/main/java/io/vertx/workshop/currency/CurrencyService.java
@@ -0,0 +1,82 @@
+package io.vertx.workshop.currency;
+
+import io.vertx.codegen.annotations.Nullable;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+
+import java.util.Random;
+
+/**
+ * @author <a href="http://escoffier.me">Clement Escoffier</a>
+ */
+public class CurrencyService extends AbstractVerticle {
+
+    private Random random = new Random();
+
+    @Override
+    public void start(Future<Void> future) throws Exception {
+        Router router = Router.router(vertx);
+        router.get().handler(rc -> rc.response().end("OK"));
+        router.post().handler(BodyHandler.create());
+        router.post().handler(this::handle);
+
+        vertx.createHttpServer()
+            .requestHandler(router::accept)
+            .listen(config().getInteger("port", 8080),
+                ar -> future.handle(ar.mapEmpty()));
+    }
+
+    private void handle(RoutingContext rc) {
+        @Nullable JsonObject json = rc.getBodyAsJson();
+        if (json == null || json.getDouble("amount") == null) {
+            System.out.println("No content or no amount");
+            rc.fail(400);
+            return;
+        }
+
+        double amount = json.getDouble("amount");
+        String target = json.getString("currency");
+        if (target == null) {
+            target = "EUR";
+        }
+        double rate = getRate(target);
+        if (rate == -1) {
+            System.out.println("Unknown currency: " + target);
+            rc.fail(400);
+        }
+        
+        int i = random.nextInt(10);
+        if (i < 5) {
+
+            rc.response().end(new JsonObject()
+                .put("amount", convert(amount, rate))
+                .put("currency", target).encode()
+            );
+        } else if (i < 8) {
+            // Failure
+            rc.fail(500);
+        }
+        // Timeout, we don't write the response.
+    }
+
+    private double getRate(String target) {
+        if (target.equalsIgnoreCase("USD")) {
+            return 1;
+        }
+        if (target.equalsIgnoreCase("EUR")) {
+            return 0.84;
+        }
+        if (target.equalsIgnoreCase("GBP")) {
+            return 0.77;
+        }
+        return -1;
+    }
+
+    private double convert(double amount, double rate) {
+        return amount * rate;
+    }
+}
