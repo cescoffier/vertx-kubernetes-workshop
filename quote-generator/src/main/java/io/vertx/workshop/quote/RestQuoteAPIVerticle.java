bbb3cc8ef5ce10efac8ffb2a0f362d74760a4a8f
Clement Escoffier <clement.escoffier@gmail.com>
2017-09-26 11:05:32 +0200

    Update the quote generator chapter

--- quote-generator/src/main/java/io/vertx/workshop/quote/RestQuoteAPIVerticle.java
+++ quote-generator/src/main/java/io/vertx/workshop/quote/RestQuoteAPIVerticle.java
@@ -1,9 +1,13 @@
 package io.vertx.workshop.quote;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.Future;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.CompletableHelper;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.eventbus.Message;
+import io.vertx.reactivex.core.http.HttpServer;
+import io.vertx.reactivex.core.http.HttpServerResponse;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,43 +20,43 @@ public class RestQuoteAPIVerticle extends AbstractVerticle {
     private Map<String, JsonObject> quotes = new HashMap<>();
 
     @Override
-    public void start() throws Exception {
-        vertx.eventBus().<JsonObject>consumer(GeneratorConfigVerticle.ADDRESS)
-            .handler(message -> {
-                // TODO Populate the `quotes` map with the received quote
-                // Quotes are json objects you can retrieve from the message body
-                // The map is structured as follows: name -> quote
-                // ----
+    public void start(Future<Void> future) throws Exception {
+        // Get the stream of messages sent on the "market" address
+        vertx.eventBus().<JsonObject>consumer(GeneratorConfigVerticle.ADDRESS).toFlowable()
+            // TODO Extract the body of the message using `.map(msg -> {})`
+            // ----
+            //
+            // ----
+            // TODO For each message, populate the `quotes` map with the received quote. Use `.doOnNext(json -> {})`
+            // Quotes are json objects you can retrieve from the message body
+            // The map is structured as follows: name -> quote
+            // ----
+            //
+            // ----
+            .subscribe();
 
-                // ----
-            });
-
-
-        vertx.createHttpServer()
-            .requestHandler(request -> {
+        HttpServer server = vertx.createHttpServer();
+        server.requestStream().toFlowable()
+            .doOnNext(request -> {
                 HttpServerResponse response = request.response()
                     .putHeader("content-type", "application/json");
 
-                // TODO
+                // TODO Handle the HTTP request
                 // The request handler returns a specific quote if the `name` parameter is set, or the whole map if none.
-                // To write the response use: `request.response().end(content)`
-                // Responses are returned as JSON, so don't forget the "content-type": "application/json" header.
-                // If the symbol is set but not found, you should return 404.
-                // Once the request handler is set,
-
-                response
-                    .end(Json.encodePrettily(quotes));
-
+                // To write the response use: `response.end(content)`
+                // If the name is set but not found, you should return 404 (use response.setStatusCode(404)).
+                // To encode a Json object, use the `encorePrettily` method
                 // ----
 
+                // Remove this line
+                response.end(Json.encodePrettily(quotes));
+                
                 // ----
             })
-            .listen(config().getInteger("http.port", 8080), ar -> {
-                if (ar.succeeded()) {
-                    System.out.println("Server started");
-                } else {
-                    System.out.println("Cannot start the server: " + ar.cause());
-                }
-            });
+        .subscribe();
+
+        server.rxListen(config().getInteger("http.port", 8080))
+            .toCompletable()
+            .subscribe(CompletableHelper.toObserver(future));
     }
 }
--- quote-generator/src/main/solution/io/vertx/workshop/quote/RestQuoteAPIVerticle.java
+++ quote-generator/src/main/solution/io/vertx/workshop/quote/RestQuoteAPIVerticle.java
@@ -1,9 +1,13 @@
 package io.vertx.workshop.quote;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.Future;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.CompletableHelper;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.eventbus.Message;
+import io.vertx.reactivex.core.http.HttpServer;
+import io.vertx.reactivex.core.http.HttpServerResponse;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,37 +20,40 @@ public class RestQuoteAPIVerticle extends AbstractVerticle {
     private Map<String, JsonObject> quotes = new HashMap<>();
 
     @Override
-    public void start() throws Exception {
-        vertx.eventBus().<JsonObject>consumer(GeneratorConfigVerticle.ADDRESS)
-            .handler(message -> {
-                // TODO Populate the `quotes` map with the received quote
-                // Quotes are json objects you can retrieve from the message body
-                // The map is structured as follows: name -> quote
-                // ----
-                System.out.println("Got a quote (solution)");
-                JsonObject quote = message.body(); // 1
-                quotes.put(quote.getString("name"), quote); // 2
-                // ----
-            });
+    public void start(Future<Void> future) throws Exception {
+        // Get the stream of messages sent on the "market" address
+        vertx.eventBus().<JsonObject>consumer(GeneratorConfigVerticle.ADDRESS).toFlowable()
+            // TODO Extract the body of the message using `.map(msg -> {})`
+            // ----
+            .map(Message::body)
+            // ----
+            // TODO For each message, populate the `quotes` map with the received quote. Use `.doOnNext(json -> {})`
+            // Quotes are json objects you can retrieve from the message body
+            // The map is structured as follows: name -> quote
+            // ----
+            .doOnNext(json -> {
+                quotes.put(json.getString("name"), json); // 2
+            })
+            // ----
+            .subscribe();
 
-
-        vertx.createHttpServer()
-            .requestHandler(request -> {
+        HttpServer server = vertx.createHttpServer();
+        server.requestStream().toFlowable()
+            .doOnNext(request -> {
                 HttpServerResponse response = request.response()
                     .putHeader("content-type", "application/json");
 
-                // TODO
+                // TODO Handle the HTTP request
                 // The request handler returns a specific quote if the `name` parameter is set, or the whole map if none.
-                // To write the response use: `request.response().end(content)`
-                // Responses are returned as JSON, so don't forget the "content-type": "application/json" header.
-                // If the symbol is set but not found, you should return 404.
-                // Once the request handler is set,
-
+                // To write the response use: `response.end(content)`
+                // If the name is set but not found, you should return 404 (use response.setStatusCode(404)).
+                // To encode a Json object, use the `encorePrettily` method
+                // ----
+                
                 String company = request.getParam("name");
                 if (company == null) {
                     String content = Json.encodePrettily(quotes);
-                    response
-                        .end(content);
+                    response.end(content);
                 } else {
                     JsonObject quote = quotes.get(company);
                     if (quote == null) {
@@ -55,20 +62,12 @@ public class RestQuoteAPIVerticle extends AbstractVerticle {
                         response.end(quote.encodePrettily());
                     }
                 }
-
-//          response
-//              .end(Json.encodePrettily(quotes));
-
-                // ----
-
                 // ----
             })
-            .listen(config().getInteger("http.port", 8080), ar -> {
-                if (ar.succeeded()) {
-                    System.out.println("Server started");
-                } else {
-                    System.out.println("Cannot start the server: " + ar.cause());
-                }
-            });
+            .subscribe();
+
+        server.rxListen(config().getInteger("http.port", 8080))
+            .toCompletable()
+            .subscribe(CompletableHelper.toObserver(future));
     }
 }
