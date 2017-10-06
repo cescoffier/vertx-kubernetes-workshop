aabdd6a72520c7ab6797f81f55fab909ae5d1f10
Clement Escoffier <clement.escoffier@gmail.com>
2017-10-06 10:05:57 +0200

    Remove useless solution projects

--- solution/portfolio-service/src/main/java/io/vertx/workshop/portfolio/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Indicates that this module contains classes that need to be generated / processed.
- */
-@ModuleGen(name = "vertx-workshop-portfolio", groupPackage = "io.vertx.workshop.portfolio")
-package io.vertx.workshop.portfolio;
-
-import io.vertx.codegen.annotations.ModuleGen;
