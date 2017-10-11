695d50384e34d32e32763930315aa7dd56dcce23
Clement Escoffier <clement.escoffier@gmail.com>
2017-10-11 16:31:18 +0200

    Fix config location

--- quote-generator/src/main/solution/io/vertx/workshop/quote/GeneratorConfigVerticle.java
+++ quote-generator/src/main/solution/io/vertx/workshop/quote/GeneratorConfigVerticle.java
@@ -68,7 +68,7 @@ public class GeneratorConfigVerticle extends AbstractVerticle {
     }
 
     private ConfigRetrieverOptions getConfigurationOptions() {
-        JsonObject path = new JsonObject().put("path", "config.json");
+        JsonObject path = new JsonObject().put("path", "config/config.json");
         return new ConfigRetrieverOptions().addStore(new ConfigStoreOptions().setType("file").setConfig(path));
     }
 }
